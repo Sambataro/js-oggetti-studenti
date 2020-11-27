ea0facdb47f3086928eaf8e2f13d06f32ae907f0
fortunato <sambataro1990@gmail.com>
2020-11-27 16:00:30 +0100

    fatto primi due esercizi

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+$(document).ready (
+  function() {
+
+    // 1 Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
+    // var dati = [];
+    // var studente = {
+    // nome: "Fortunato",
+    // cognome: "Sambataro",
+    // eta: 27,
+    // };
+    //
+    // for (var key in studente) {
+    //   $(".stampa").append(key + ": " + studente[key] + "<br>");
+    // };
+
+    // fine esercizio 1 -----------------------
+
+    //  2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+    // var studenti = [
+    //   {
+    //     "nome": "Fortunato",
+    //     "cognome": "Sambataro",
+    //   },
+    //   {
+    //     "nome": "Franco",
+    //     "cognome": "Franchi",
+    //   },
+    // ]
+    //
+    // for(var i = 0; i < studenti.length; i++) {
+    //   for (key in studenti[i]) {
+    //     if(key == "nome") {
+    //       $(".stampa").append("utente: " + studenti[i][key] + "<br>");
+    //     } else if (key == "cognome"){
+    //       $(".stampa").append(studenti[i][key] + "<br>");
+    //     } else {
+    //       console.log(studenti[i]);
+    //     }
+    //   }
+    // }
+
+
+    // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+    var dati = [];
+    var studente = {
+    nome: "Fortunato",
+    cognome: "Sambataro",
+    eta: 27,
+    };
+
+    for (var key in studente) {
+      ;
+    };
+
+
+
+
+
+  }
+)
